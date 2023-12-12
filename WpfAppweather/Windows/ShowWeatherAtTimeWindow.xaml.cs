0f40b3c7d8dfaaee8976992ab452709d1a14fe63
filonov.vanya@gmail.com <filonov.vanya@gmail.com>
2023-12-12 16:06:18 +0300

    Добавьте файлы проекта.

---
+++ WpfAppweather/Windows/ShowWeatherAtTimeWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppweather.Classes;
+using WpfAppweather.CustomControls;
+using WpfAppweather.DataModel;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace WpfAppweather.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowWeatherAtTimeWindow.xaml
+    /// </summary>
+    public partial class ShowWeatherAtTimeWindow : Window
+    {
+        private Prognosis prognosis;
+        public ShowWeatherAtTimeWindow(Prognosis prog)
+        {
+            InitializeComponent();
+            prognosis = prog;
+            labelContent.Content += " " + prognosis.Date.Value.ToShortDateString();
+            LoadDataWrapPanel(prog.Time);
+        }
+
+        private void LoadDataWrapPanel(string info)
+        {
+            List<ComplexWeatherInfo> infos;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<ComplexWeatherInfo>));
+            using (StringReader reader = new StringReader(info))
+            {
+                infos = xmlSerializer.Deserialize(reader) as List<ComplexWeatherInfo>;
+            }
+
+            infos.ForEach(ItemCollection => wrapPanelWeather.Children.Add(new WeatherCustomControl(ItemCollection)));
+        }
+    }
+}
